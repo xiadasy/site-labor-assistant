5e20614847a83e6c60cadc534300ea9eac5dea58
zq <a15221082942@gmail.com>
2026-07-10 21:30:29 +0800

    feat: publish sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='dongfang-labor-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./app-icon.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{
+  if(new URL(event.request.url).pathname.endsWith('encrypted-data.json')){
+    event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response})));
+});
