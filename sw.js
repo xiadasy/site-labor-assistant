2b11ce157e56438b5f73edc0ff5c0ac06df65eba
zq <a15221082942@gmail.com>
2026-07-10 22:39:21 +0800

    feat: publish sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dongfang-labor-v1';
+const CACHE='dongfang-labor-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./app-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
