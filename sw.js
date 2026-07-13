47050508e58eb33fa3aa79e9637d76749f80780d
zq <a15221082942@gmail.com>
2026-07-13 13:12:12 +0800

    chore: refresh PWA cache for payroll import fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dongfang-labor-v9';
+const CACHE='dongfang-labor-v10';
 const ASSETS=['./','./index.html','./admin.html','./company.html','./company-admin.html','./manifest.webmanifest','./app-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
