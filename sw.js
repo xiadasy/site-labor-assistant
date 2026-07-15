d91784393e54f21dcc19f793127e281380fc7024
zq <a15221082942@users.noreply.github.com>
2026-07-15 11:20:06 +0800

    fix: load latest project UI and populate unit filter

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-const CACHE='dongfang-labor-v22';
+const CACHE='dongfang-labor-v23';
 const ASSETS=['./','./index.html','./admin.html','./company.html','./company-admin.html','./project.html','./manifest.webmanifest','./app-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>{
-  if(new URL(event.request.url).pathname.endsWith('encrypted-data.json')||new URL(event.request.url).pathname.endsWith('project-encrypted-data.json')){
+  const path=new URL(event.request.url).pathname;
+  if(path.endsWith('/project.html')){
+    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)));
+    return;
+  }
+  if(path.endsWith('encrypted-data.json')||path.endsWith('project-encrypted-data.json')){
     event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
     return;
   }
