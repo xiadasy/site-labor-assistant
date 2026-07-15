bcd0900dc676309a55894a829ab37765ea6e5600
zq <a15221082942@users.noreply.github.com>
2026-07-15 14:51:03 +0800

    feat: publish first batch of real worker dossiers

--- sw.js
+++ sw.js
@@ -1,16 +1,11 @@
-const CACHE='dongfang-labor-v23';
-const ASSETS=['./','./index.html','./admin.html','./company.html','./company-admin.html','./project.html','./manifest.webmanifest','./app-icon.png'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener('fetch',event=>{
-  const path=new URL(event.request.url).pathname;
-  if(path.endsWith('/project.html')){
-    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)));
-    return;
-  }
-  if(path.endsWith('encrypted-data.json')||path.endsWith('project-encrypted-data.json')){
-    event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response})));
+const CACHE='labor-v25';
+const ASSETS=['./','./index.html','./project.html','./company.html','./encrypted-data.json','./project-encrypted-data.json','./company-encrypted-data.json','./widget-summary.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ const u=new URL(e.request.url);
+ if(u.pathname.endsWith('/project.html')||u.pathname.endsWith('/project-encrypted-data.json')){
+  e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));return;
+ }
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
