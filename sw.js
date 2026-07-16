c4244ae5a35be70bf71b27df6eb87c80c1505723
zq <a15221082942@users.noreply.github.com>
2026-07-16 08:52:07 +0800

    fix: use unobtrusive leader watermark on project roster

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='labor-v26';
+const CACHE='labor-v27';
 const ASSETS=['./','./index.html','./project.html','./company.html','./encrypted-data.json','./project-encrypted-data.json','./company-encrypted-data.json','./widget-summary.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
